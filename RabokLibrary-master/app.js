5645a6dd43c813aee00dfd85ce631643c9a653bd
Damarice <menganyidamarice@gmail.com>
2026-05-12 10:13:56 +0300

    Remove duplicate RabokLibrary-master folder after integration

--- RabokLibrary-master/app.js
+++
@@ -1,9 +0,0 @@
-/**
- * Production Entry Point for cPanel Node.js App
- * This file is required for cPanel Node.js hosting
- */
-
-const app = require('./server');
-
-// Export the app for cPanel
-module.exports = app;
